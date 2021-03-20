e5f7bd271b887b9fed02b28a1d9a8674e59c3a9d
Kajal Patel <kajalpatel20@gmail.com>
2021-03-20 00:08:23 -0400

    made a change

--- employee.js
+++ employee.js
@@ -10,7 +10,7 @@ const connection = mysql.createConnection({
     host: 'localhost',
     port: 3306,
     user: 'root',
-    password: '',
+    password: 'Shaan0807',
     database: 'employees_db',
 })
 
@@ -160,19 +160,125 @@ function addDepartment() {
             type: 'input',
             message: 'Which department would you like add?',
         }
-    ]).then (function(answer) {
+    ]).then(function (answer) {
         connection.query(
             'INSERT INTO department SET ?',
             {
                 name: answer.newDepartment
             })
         var query = 'select * from department';
-        connection.query(query,function(err, res) {
+        connection.query(query, function (err, res) {
             if (err) throw err;
             console.log("your department has been added");
             console.table("All Departments: ", res);
             options();
-        } )
+        })
     })
+}
+function addRole() {
+    connection.query("SELECT * FROM department", function (err, res) {
+        if (err) throw err;
+        inquirer.prompt([
+            {
+                name: 'new_role',
+                type: 'input',
+                message: 'What new role would you like add?'
+            },
+            {
+                name: 'salary',
+                type: 'input',
+                message: 'What is the salary of this role? (enter a number)'
+            },
+            {
+                name: 'Department',
+                type: 'list',
+                choices: function () {
+                    var departmentArray = [];
+                    for (let i = 0; i < res.length; i++) {
+                        departmentArray.push(res[i].name);
+                    }
+                    return departmentArray
+                }
+            }
+        ]).then(function (answer) {
+            let department_id;
+            for (let a = 0; a < res.length; a++) {
+                if (res[a].name == answer.Department) {
+                    department_id = res[a].id
+                }
+            }
+            connection.query('INSERT INTO role SET ?',
+                {
+                    title: answer.new_role,
+                    salary: answer.salary,
+                    department_id: department_id
+                },
+                function (err, res) {
+                    if (err) throw err;
+                    console.log('your new role has been added!');
+                    console.table('All roles : ', res);
+                    options();
+                }
+            )
+        })
+    });
+}
+function updateEmployeeRole() {
+    let employeeArray = [];
+    let roleArray = [];
+    connection.query('SELECT id, title FROM role ORDER BY title ASC', function (err, res) {
+        if (err) throw err;
+        for (i = 0; i < res.length; i++) {
+            roleArray.push(res[i].title);
+        }
+    })
+
+    connection.query("SELECT employee.id, concat(employee.first_name, ' ' ,  employee.last_name) AS Employee FROM employee ORDER BY Employee ASC", function (err, res) {
+        if (err) throw err;
+        for (i = 0; i < res.length; i++) {
+            employeeArray.push(res[i].Employee);
+        }
+    })
+
+
+    inquirer.prompt([
+        {
+            name: 'employee',
+            type: 'list',
+            message: 'What would you like to edit?',
+            choices: employeeArray
+
+        },
+        {
+            name: 'role',
+            type: 'list',
+            message: 'What is their new role?',
+            choices: roleArray
+        },
+    ]).then((answer) => {
+        let roleID;
+        let employeeID;
+        for (i = 0; i < roles.length; i++) {
+            if (answer.role == roles[i].title) {
+                roleID = roles[i].id
+            }
+        }
+        for (i = 0; i < employees.length; i++) {
+            if (answer.employee == employees[i].Employee) {
+                employeeID = employees[i].id
+            }
+        }
+        connection.query('UPDATE employee SET role_id = ? where id = ?', {
+            role_id: roleID,
+            id: employeeID,
+        },
+            function (err) {
+                if (err) throw err;
+                console.log("employee role has been updated!")
+                options();
+            })
+    })
+
+
 }
 
