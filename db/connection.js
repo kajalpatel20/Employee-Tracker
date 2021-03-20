46ded38097046e2d45ee7b4ae033afa4fb5292d7
Kajal Patel <kajalpatel20@gmail.com>
2021-03-20 00:10:05 -0400

    delet

--- db/connection.js
+++ db/connection.js
@@ -4,7 +4,7 @@ const mysql = require('mysql');
 const connection = mysql.createConnection({
     host: 'localhost',
     user: 'root',
-    password: 'Shaan0807',
+    password: '',
     database: 'employees_db'
 });
 
--- employee.js
+++ employee.js
@@ -10,7 +10,7 @@ const connection = mysql.createConnection({
     host: 'localhost',
     port: 3306,
     user: 'root',
-    password: 'Shaan0807',
+    password: '',
     database: 'employees_db',
 })
 
